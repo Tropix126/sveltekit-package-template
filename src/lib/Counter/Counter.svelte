44b63f545770c08dd9fcdde343d851d80c16efb9
Tropical <42101043+Tropix126@users.noreply.github.com>
2021-12-16 00:08:25 -0600

    [feat] Add component template documentation.

--- src/lib/Counter/Counter.svelte
+++ src/lib/Counter/Counter.svelte
@@ -7,6 +7,15 @@
     export { className as class }; // "class" is a reserved namespace in JS
 </script>
 
+<!--
+@component
+A component that controls the value of a given number. Clicking the + button will add one to the number. Clicking the - button will subtract one from the number.
+
+- Usage:
+    ```tsx
+    <Counter value={0} class="my-custom-class" />
+    ```
+-->
 <div class="counter {className}" {...$$restProps}>
     <button on:click={() => value--}>
         -
