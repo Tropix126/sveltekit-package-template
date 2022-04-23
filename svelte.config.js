545a020b24d75c43a73e81d09ca42fdf6dad2845
Tropical <42101043+Tropix126@users.noreply.github.com>
2022-04-22 19:25:33 -0500

    Remove `config.kit.target`

--- svelte.config.js
+++ svelte.config.js
@@ -10,12 +10,7 @@ const config = {
 		postcss: {
 			plugins: [autoprefixer(), cssnano()]
 		}
-	}),
-
-	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
-	}
+	})
 };
 
 export default config;
